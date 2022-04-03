edd57b0fad437058171502591d42c203feff3d61
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2022-04-03 22:26:37 +0200

    modify important file

--- script.js
+++ script.js
@@ -147,23 +147,6 @@ function handleClick(event) {
     });
 }
 
-console.log("Getting data")
-
-
-
-
-
-var docRef = db.collection("users").doc("aqPNEN79hPbg2VwFYksBdr4mNff2").collection("events").doc("1648900639100");
-docRef.get().then((doc) => {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
 
 
 /********************************************************************
