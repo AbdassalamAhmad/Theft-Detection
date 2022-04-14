598c75e31c3028ece205e2f0293daeb20a019c63
AbdAssalam Ahmad <abdassalam.ahmad98@gmail.com>
2022-04-14 23:40:44 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,21 @@
 // Import and configure the Firebase SDK
 import { initializeApp } from 'firebase/app';
 import { getMessaging } from 'firebase/messaging/sw';
-import { getFirebaseConfig } from './firebase-config';
 
-const firebaseApp = initializeApp(getFirebaseConfig());
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAqu-usFbiECu0VqE_z-4GVlkYGfY9PYIo",
+    authDomain: "person-detection-9ba5d.firebaseapp.com",
+    projectId: "person-detection-9ba5d",
+    storageBucket: "person-detection-9ba5d.appspot.com",
+    messagingSenderId: "865499889032",
+    appId: "1:865499889032:web:e4515efb6f29dd6d1d8e3d"
+};
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
 getMessaging(firebaseApp);
 console.info('Firebase messaging service worker is set up');
+
+
